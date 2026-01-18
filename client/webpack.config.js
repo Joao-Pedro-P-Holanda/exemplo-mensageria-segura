eb986ef3c250990a2bff4d369cba314e157f907e
José Souza <joseedsouza.dev@gmail.com>
2026-01-18 13:08:00 -0300

    chore: reformat webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,46 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path")
+const Dotenv = require("dotenv-webpack")
 
 module.exports = {
-  mode: 'development',
-  dotenv: {
-    prefix: 'WEBPACK_',
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    port: 9000,
-  },
-  entry: './src/app.js',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin(
-      { filename: 'styles.css' }
-    ),
-    new Dotenv()
-  ],
-  module: {
-
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.pem$/i,
-        type: 'asset/source'
-      }
-    ],
-  },
-};
+	mode: "development",
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		port: 9000,
+	},
+	entry: "./src/app.js",
+	output: {
+		filename: "app.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+			filename: "index.html",
+		}),
+		new MiniCssExtractPlugin({ filename: "styles.css" }),
+		new Dotenv(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.pem$/i,
+				type: "asset/source",
+			},
+		],
+	},
+}
